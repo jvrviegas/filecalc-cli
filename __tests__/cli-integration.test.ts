1a7052476c9338c93619d8615a6ce9a08b234389
João Vitor Viegas Rêgo <jvrviegas@gmail.com>
2021-02-16 09:43:34 -0300

    ✅ - Implement commands tests

--- __tests__/cli-integration.test.ts
+++ __tests__/cli-integration.test.ts
@@ -1,29 +1,43 @@
-import { system, filesystem } from 'gluegun'
+import { system, filesystem } from 'gluegun';
 
-const src = filesystem.path(__dirname, '..')
+const src = filesystem.path(__dirname, '..');
 
 const cli = async (cmd) =>
-  system.run(`node ${filesystem.path(src, 'bin', 'filecalc')} ${cmd}`)
+  system.run(`node ${filesystem.path(src, 'bin', 'filecalc')} ${cmd}`);
 
 test('outputs version', async () => {
-  const output = await cli('--version')
-  expect(output).toContain('0.0.1')
-})
+  const output = await cli('--version');
+  expect(output).toContain('0.0.1');
+});
 
 test('outputs help', async () => {
-  const output = await cli('--help')
-  expect(output).toContain('0.0.1')
-})
+  const output = await cli('--help');
+  expect(output).toContain('0.0.1');
+});
 
-test('generates file', async () => {
-  const output = await cli('generate foo')
+test('outputs cars count based on brand', async () => {
+  const output = await cli('brand tesla');
+  expect(output).toContain('Cars counted succesfully');
+  expect(output).toContain('Number of TESLA cars:');
+});
 
-  expect(output).toContain('Generated file at models/foo-model.ts')
-  const foomodel = filesystem.read('models/foo-model.ts')
+test('outputs cars count and list based on brand', async () => {
+  const output = await cli('brand tesla --list');
+  expect(output).toContain('Brand');
+  expect(output).toContain('Dealership');
+  expect(output).toContain('Mileage');
+  expect(output).toContain('Value');
+  expect(output).toContain('Cars counted succesfully');
+});
 
-  expect(foomodel).toContain(`module.exports = {`)
-  expect(foomodel).toContain(`name: 'foo'`)
+test('outputs cars count based on mileage range', async () => {
+  const output = await cli('mileage 10000 12000');
+  expect(output).toContain('Cars counted succesfully');
+  expect(output).toContain('km');
+});
 
-  // cleanup artifact
-  filesystem.remove('models')
-})
+test('outputs cars total value on a dealership', async () => {
+  const output = await cli('dealership "Auto Machado"');
+  expect(output).toContain('Cars values succesfully summed');
+  expect(output).toContain('€');
+});
